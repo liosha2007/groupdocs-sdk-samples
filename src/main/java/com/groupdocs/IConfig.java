343432ef0b1176d006f6142483054d95665fe5d5
aleksey.permyakov <aleksey.permyakov@groupdocs.com>
2013-03-06 11:51:32 +0200

    Updated

--- src/main/java/com/groupdocs/IConfig.java
+++ src/main/java/com/groupdocs/IConfig.java
@@ -1,8 +1,8 @@
 package com.groupdocs;
 
 public interface IConfig {
-	public static final String CID = "b912b34d1621a895";
-	public static final String PKEY = "701b7b219b50dfa23b0b513d03e9f0d0";
+	public static final String CID = "12345";
+	public static final String PKEY = "1234567890";
 	public static final String BASE_PATH = "https://api.groupdocs.com/v2.0";
 	public static final String FILE_TO_UPLOAD = "./target/classes/file_to_upload_groupdocs-sdk-samples.txt";
 	public static final String DOCUMENT_TO_SIGN = "./target/classes/document_to_sign.docx";
